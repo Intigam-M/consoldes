b41c96822a4a98c18762896852c70800488c7e23
Narmin-G-M <narmin.g.mammadli@gmail.com>
2023-08-02 07:43:28 +0400

    Complete Hero Component

--- frontend/components/header/index.js
+++ frontend/components/header/index.js
@@ -30,11 +30,11 @@ function Header() {
     };
 
     return (
-        <header className='bg-[#b8dde3]'>
+        <header className='bg-[#9584FF]'>
             <nav className='p-5 md:flex md:items-center md:justify-between container'>
                 <div className='flex justify-between items-center'>
                     <Image src={Logo} alt="hero" width={40} height={40} />
-                    <span className='text-2xl cursor-pointer font-bold text-slate-700 tracking-wider pl-3 text-[1.2rem]'>CONSOLDES</span>
+                    <span className='text-2xl cursor-pointer font-bold text-white tracking-wider pl-3 text-[1.2rem]'>CONSOLDES</span>
                     {
                         isMenuOpen ? <GrClose size={30} onClick={toggleMenu} className='cursor-pointer md:hidden block mx-2' />
                             : <GiHamburgerMenu size={30} onClick={toggleMenu} className='cursor-pointer md:hidden block mx-2' />
@@ -42,19 +42,19 @@ function Header() {
                 </div>
                 <ul className={`md:flex md:items-center z-[-1] md:z-auto md:static absolute w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-2 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-200 ${isMenuOpen ? "top-[70px] opacity-100" : ""}`}>
                     <li className='mx-4 my-6 md:my-0'>
-                        <Link href={`/${currentLang()}`} className='text-xl hover:text-red-800'>Home</Link>
+                        <Link href={`/${currentLang()}`} className='text-xl text-white hover:text-red-800'>Home</Link>
                     </li>
                     <li className='mx-4 my-6 md:my-0'>
-                        <Link href={`/${currentLang()}/course`} className='text-xl hover:text-red-800'>Courses</Link>
+                        <Link href={`/${currentLang()}/course`} className='text-xl text-white hover:text-red-800'>Courses</Link>
                     </li>
                     <li className='mx-4 my-6 md:my-0'>
-                        <Link href={`/${currentLang()}/blog`} className='text-xl hover:text-red-800'>Blog</Link>
+                        <Link href={`/${currentLang()}/blog`} className='text-xl text-white hover:text-red-800'>Blog</Link>
                     </li>
                     <li className='mx-4 my-6 md:my-0'>
-                        <Link href={`/${currentLang()}/about`} className='text-xl hover:text-red-800'>About</Link>
+                        <Link href={`/${currentLang()}/about`} className='text-xl text-white hover:text-red-800'>About</Link>
                     </li>
                     <li className='mx-4 my-6 md:my-0'>
-                        <Link href={`/${currentLang()}/contact`} className='text-xl hover:text-red-800'>Contact</Link>
+                        <Link href={`/${currentLang()}/contact`} className='text-xl text-white hover:text-red-800'>Contact</Link>
                     </li>
                     
                 </ul>
--- frontend/components/homePage/hero/index.js
+++ frontend/components/homePage/hero/index.js
@@ -5,18 +5,18 @@ import hero from '../../../assets/images/hero.png'
 function Hero({ dictionary }) {
     console.log(dictionary);
     return (
-        <div className='bg-[#b8dde3]'>
-            <div className='container flex flex-col md:flex-row'>
-                <div className='md:w-1/2 w-full grid items-center justify-items-center md:justify-items-start'>
+        <div className='bg-[#9584FF]'>
+            <div className='container flex flex-col md:flex-row gap-10'>
+                <div className='md:w-5/12 w-full grid items-center justify-items-center md:justify-items-start'>
                     <div>
-                        <h1 className='text-5xl font-bold pb-5 leading-tight'>
+                        <h1 className='text-5xl font-bold pb-5  text-white  leading-tight'>
                           {dictionary.heroTitle}
                         </h1>
-                        <p className='font-bold tracking-wide text-stone-700'>{dictionary.heroSpan}</p>
+                        <p className='font-bold tracking-wide text-white text-opacity-60'>{dictionary.heroSpan}</p>
                     </div>
                 </div>
-                <div className='md:w-1/2 w-full h-[800px] overflow-hidden'>
-                    <Image src={hero} alt="hero" width={800} height={500} />
+                <div className='md:w-7/12 w-full h-[700px] grid overflow-hidden items-center  '>
+                    <Image src={hero} alt="hero" width={700} height={500} />
                 </div>
             </div>
         </div>
